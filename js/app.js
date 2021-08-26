5bbf6fe5708b76e92c05f82e9115091e3b35946f
kuzh69 <70613242+kuzh69@users.noreply.github.com>
2021-08-25 21:24:54 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,284 +1,273 @@
-window.onload = function(){
+window.onload = function() {
     btnRegistrar = document.getElementById("btnRegistrar");
     ingreso = document.getElementById("ingreso");
     registro = document.getElementById("registro");
-    ingreso = document.getElementById("principal");
+    principal = document.getElementById("principal");
     txtCorreo = document.getElementById("correoR");
     txtNombre = document.getElementById("nombreR");
     txtContrasena = document.getElementById("contrasenaR");
     txtConfirmacion = document.getElementById("confirmacionR");
     txtFecha = document.getElementById("fechaR");
     btnRegistro = document.getElementById("btnRegistro");
-    btnRegresarIngresar = document.getElementById("Entrar");
-    txtCorreoI = document.getElementById("correoI")
+    btnIngresar = document.getElementById("btnIngresar");
+    txtCorreoI = document.getElementById("correoI");
     txtContrasenaI = document.getElementById("contrasenaI");
     nombreP = document.getElementById("nombreP");
-    MensajeM = document.getElementById("mensajeM");
-    correoM = document.getElementById("correoM");
-    photo = document.getElementById("photo");
-    camara = document.getElementById("camara");
-    openq = document.getElementById("open");
-    mapa = document.getElementById("mapa");
-    lugar = document.getElementById("lugar");
+    btnenviarM = document.getElementById("enviarM");
+    txtCorreoM = document.getElementById("correoM");
+    txtMensajeM = document.getElementById("mensajeM");
     redactar = document.getElementById("redactar");
-    mensajes = document.getElementById("mensajes");
-    /*localStorage.setItem("login", 1);
-    localStorage.setItem("nombre", "Jose M");
-    localStorage.setItem("correo", "1@1.com");
-    if (localStorage.getItem("login") !=="1"){
-        ingreso.style.display="block";
-        principal.style.display="none";
-        redactar.style.display="none";
-        document.getElementById("camara").style.display = "none";
-    } 
-    else{
-        ingreso.style.display="none";
-        principal.style.display="block";
-        redactar.style.display="block";
-        nombreP.innerHTML= "Jose M";
+    photo = document.getElementById("photo");
+    camera = document.getElementById("camera");
+    open = document.getElementById("open");
+    if (localStorage.getItem("login") !== "1") {
+        ingreso.style.display = "block";
+        principal.style.display = "none";
+        redactar.style.display = "none";
+    }
+    else {
+        ingreso.style.display = "none";
+        principal.style.display = "block";
+        redactar.style.display = "block";
         nombre = localStorage.getItem("nombre");
-        nombre = localStorage.getItem("correo");
-        //leerM();
-    }*/
+        correo = localStorage.getItem("correo");
+        document.getElementById("nombreP").innerHTML = nombre;
+        leerM();
+    }
 
-}
-enviarM.addEventListener("click", function(){
-    if(correoM.value == ""){
-        alert ("Debes escribir usuario");
-        correoM.classList.add("errorCampo");
+
+document.getElementById("enviarM").addEventListener("click", function() {
+    if (txtCorreoM.value == "") {
+        alert("Debe escribir el correo");
+        txtCorreoM.classList.add("errorCampo");
         return false;
     }
     else {
-        correoM.classList.remove("errorCampo");
+        txtCorreoM.classList.remove("errorCampo");
     }
-    if(MensajeM.value == ""){
-        alert ("Debes escribir el mensaje");
-        MensajeM.classList.add("errorCampo");
+
+    if (txtMensajeM.value == "") {
+        alert("Debe escribir el nombre");
+        txtMensajeM.classList.add("errorCampo");
         return false;
     }
     else {
-        correoM.classList.remove("errorCampo");
+        txtMensajeM.classList.remove("errorCampo");
     }
-    let metaMensaje = new FormData();
-    metaMensaje.append("correoM", correoM.value);
-    metaMensaje.append("MensajeM", MensajeM.value);
 
-    fetch ("http://tcpmgja.orgfree.com/registrarMensaje.php", {
-        method: 'POST',
-        body: metaMensaje
+    let datosM = new FormData();
+    datosM.append("correoM", txtCorreoM.value);
+    datosM.append("mensajeM", txtMensajeM.value);
+
+    fetch("https://tallerprogramacionavanzada.000webhostapp.com/guardarMensaje.php", {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        body: datosM
     })
-    .then (function(response){
+    .then(function (response) {
         if (response.ok) {
-            alert ("Mensaje Enviado");
+            alert("Usuario registrado");
         }
         else {
-            alert("Ocurrio un error");
+            alert("Ocurrió un error al registrar");
             console.log(response);
         }
-        })
-        .catch(function(err) {
-            alert("ocurrio un error");
-            console.log(err);
-        }); 
+    })
+    .catch(function(err) {
+        alert("Ocurrió un error inesperado");
+        console.log(err);
+    });
 });
 
-btnRegistrar.addEventListener("click", function(){
-ingreso.style.display ="none";
-registro.style.display ="block";
+btnRegistrar.addEventListener("click", function() {
+    ingreso.style.display = "none";
+    registro.style.display = "block";
 });
 
-btnRegistro.addEventListener("click", function(){
-
-    if(txtCorreo.value == ""){
-        alert ("Debes escribir tu correo");
+btnRegistro.addEventListener("click", function() {
+    if (txtCorreo.value == "") {
+        alert("Debe escribir el correo");
         txtCorreo.classList.add("errorCampo");
         return false;
     }
     else {
         txtCorreo.classList.remove("errorCampo");
     }
-    if(txtContrasena.value !== txtConfirmacion.value){
-        alert ("La confirmacion de la Contraseña es erronea");
-        txtConfirmacion.classList.add("errorCampo");
-        txtContrasena.classList.add("errorCampo");
+
+    if (txtNombre.value == "") {
+        alert("Debe escribir el nombre");
         return false;
     }
+    if (txtContrasena.value == "") {
+        alert("Debe escribir la contraseña");
+        return false;
+    }
+    if (txtConfirmacion.value == "") {
+        alert("Debe escribir la confirmación");
+        return false;
+    }
+    if (txtContrasena.value !== txtConfirmacion.value) {
+        alert("La contraseña y confirmación no coincide");
+        return false;
+    }
+    if (txtFecha.value == "") {
+        alert("Debe escribir la fecha");
+        return false;
+    }
+
     let datos = new FormData();
     datos.append("correoR", txtCorreo.value);
     datos.append("nombreR", txtNombre.value);
     datos.append("contrasenaR", txtContrasena.value);
     datos.append("fechaR", txtFecha.value);
-
-    fetch ("http://tcpmgja.orgfree.com/registro.php", {
-        method: 'POST',
+    //fetch("http://tpaepg.orgfree.com/registro.php", {
+    fetch("https://tallerprogramacionavanzada.000webhostapp.com/registro.php", {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
         body: datos
     })
-    .then (function(response){
+    .then(function (response) {
         if (response.ok) {
-            alert ("usuario registrado");
+            alert("Usuario registrado");
         }
         else {
-            alert("Ocurrio un error");
+            alert("Ocurrió un error al registrar");
             console.log(response);
         }
-        })
-        .catch(function(err) {
-            alert("ocurrio un error");
-            console.log(err);
-        }); 
+    })
+    .catch(function(err) {
+        alert("Ocurrió un error inesperado");
+        console.log(err);
+    });
 });
-document.getElementById("Entrar").addEventListener("click", function () {
-    if(txtCorreoI.value == ""){
-        alert ("Debes escribir tu correo");
-        txtCorreoI.classList.add("errorCampo");
+
+btnIngresar.addEventListener("click", function () {
+    if (txtCorreoI.value == "") {
+        alert("Debe escribir el correo");
+        txtCorreo.classList.add("errorCampo");
         return false;
     }
     else {
         txtCorreoI.classList.remove("errorCampo");
     }
-    if(txtContrasenaI.value == ""){
-        alert ("Debes escribir tu contrasena");
-        txtContrasenaI.classList.add("errorCampo");
+
+    if (txtContrasenaI.value == "") {
+        alert("Debe escribir la contraseña");
         return false;
     }
     else {
-        txtCorreoI.classList.remove("errorCampo");
+        txtContrasenaI.classList.remove("errorCampo");
     }
+
     let datosI = new FormData();
     datosI.append("correoI", txtCorreoI.value);
     datosI.append("contrasenaI", txtContrasenaI.value);
 
-    fetch ("http://tcpmgja.orgfree.com/Ingreso.php", {
-        method: 'POST',
+    //fetch("http://tpaepg.orgfree.com/ingreso.php", {
+    fetch("https://tallerprogramacionavanzada.000webhostapp.com/ingreso.php", {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
         body: datosI
     })
-    .then (function(response){
+    .then(function (response) {
         return response.json();
     })
-    .then (function(data){
-        if(data.fallo =="contrasena")
-        {alert("debe ingresar contraseña")}
-        else {  nombre = data.nombre;
-                correo = data.correo;
-                document.getElementById("ingreso").style.display = "none";
-                principal.style.display = "block";
-                nombreP.innerHTML = nombre;
-                localStorage.setItem("Login", 1);
-                localStorage.setItem("nombre", nombre);
-                localStorage.setItem("correo", correo);
-                leerM();
-
-            }
-    })
-        .catch(function(err) {
-            alert("ocurrio un error");
-            console.log(err);
-        });
-
-});
-
-
-function abrirBarra(){
-    document.getElementById("barraMenu").style.width = "250px";
-}
-function cerrarBarra(){ 
-    document.getElementById("barraMenu").style.width= "0px";
-}
-        
-
-function leerM()
-{   //Inicio leerM
-        let datosLM = new FormData();
-        datosLM.append("correoUsuario", correo)
-        fetch ("http://tcpmgja.orgfree.com/leerMensajes.php", {
-        method: 'POST',
-        body: datosLM
-    })
-    .then (function(response){
-        return response.json();
-    })
-    .then (function(data){
-        for ( let x= 0; x < data.length;){
-            x++;
-            document.getElementById("mensajes").innerHTML = document.getElementById("mensajes").innerHTML + data[x].correo + "<br>" + data[x].mensaje + "<br>" +data[x].fechahora+ "<br>";
+    .then(function(data){
+        if (data.fallo == "contrasena") {
+            alert("Debe escribir la contraseña correcta");
         }
+        else if (data.fallo == "usuario") {
+            alert("El correo no está registrado");
+        }
+        else {
+            nombre = data.nombre;
+            correo = data.correo;
+            ingreso.style.display = "none";
+            principal.style.display = "block";
+            nombreP.innerHTML = nombre;
+            localStorage.setItem("login", 1);
+            localStorage.setItem("nombre", nombre);
+            localStorage.setItem("correo", correo);
+            leerM();
+        }
+   })
+    .catch(function(err) {
+        alert("Ocurrió un error inesperado");
+        console.log(err);
     });
-}//fin leerM
-
-document.getElementById("open").addEventListener("click", function(){
-    document.getElementById("camera").click();
 });
 
-function tomarfoto() {
-    redactar.style.display = "none";
-    mensajes.style.display = "none";
-    document.getElementById("camara").style.display = "block";
-}
+document.getElementById("open").addEventListener("click", function() {
+    camera.click();
+});
 
-document.getElementById("camera").addEventListener("change", function(e){
-    ruta = URL.createObjectURL(e.target.file[0]);
-    //7photo.src = URL.createObjectURL(e.target.file[0]);
-    obtenerLugar();
-    photo.src = ruta;
-    if (obtenerSO() =="iOS"){
-        let link = document.createElement('a');
-        link.download= "prueba1.png";
-        //link.href = ruta; ///photo.toDataURL(images/png).replace("image/png", "image/octet-stream");
-        link.ruta;
-        link.click();
-        alert("foto capturada");
-    }
+camera.addEventListener("change", function(e) {
+    photo.src = URL.createObjectURL(e.target.files[0]);
+//    photo.srcObject = URL.createObjectURL(e.target.files[0]);
+    let link = document.createElement('a');
+    link.download = "test.png";
+    link.href = photo.toDataURL("image/png").replace("image/png", "image/octet-stream");
+    link.click();
+});
     
-});
+}  //onload
 
-mapa.addEventListener('click', function(){
-    window.open("http://nominatim.openstreetmap.org/?mlat=" + coordenadas.lat + "&mlon" + coordenadas.lon + "&zoom=20");
-});
-
-function obtenerLugar(){
-coordenadas = {lat:0, lon:0};
-navigator.geolocation.getCurrentPosition(function(position){
-    coordenadas = {lat: position.coords.latitude, lon: position.coords.longitude}
-
-    fetch ("https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=" + coordenadas.lat + "&lon" + coordenadas.lon)
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById("lugar").value = data.address.country + " " + data.address.state;
-    })
-    .catch(error => {
-        console.log(error);
-        coordenadas = {lat:0, lon:0};
-    });
-});
-}
-
-function obtenerSO(){
-    let so = null;
-    let platform = window.navigator.platform,
-        iosPlatforms = ['iPhone', 'iPad','iPod'];
-    if (iosPlatforms.includes(platforms)){
-        so = 'iOS';
-    return so;
-    }
-}
-
-function cerrarSesion() {
-
-    cerrarBarra();
-    localStorage.removeItem("nombre");
-    localStorage.removeItem("correo");
-    localStorage.setItem("login", 0);
-
-    redactar.style.display = "none";
-    document.getElementById("principal").style.display= "none";
-    document.getElementById("mensajes").style.display= "none";
-    document.getElementById("camara").style.display= "none";
-    document.getElementById("ingreso").style.display= "block";
-}
 function mensajes() {
     redactar.style.display = "block";
     document.getElementById("mensajes").style.display = "block";
     document.getElementById("camara").style.display = "none";
     cerrarBarra();
-}
+} //principal
 
+function cerrarSesion() {
+    cerrarBarra();
+    localStorage.removeItem("nombre");
+    localStorage.removeItem("correo");
+    localStorage.setItem("login", 0);
+    //localStorage.clear();
+    redactar.style.display = "none";
+    document.getElementById("principal").style.display = "none";
+    document.getElementById("mensajes").style.display = "none";
+    document.getElementById("camara").style.display = "none";
+    document.getElementById("ingreso").style.display = "block";
+} //cerrarSesion
+
+function abrirBarra() {
+    document.getElementById("barraMenu").style.width = "250px";
+} //abrirBarra
+
+function cerrarBarra() {
+    document.getElementById("barraMenu").style.width = "0";
+} //cerrarBarra
+
+function leerM() {
+    let datosLM = new FormData();
+    datosLM.append("correoUsuario", correo);
+    //fetch("http://tpaepg.orgfree.com/leerMensajes.php", {
+    fetch("https://tallerprogramacionavanzada.000webhostapp.com/leerMensajes.php", {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        body: datosLM
+    })
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function(data){
+        for (let x = 0; x < data.length; x++) {
+            document.getElementById("mensajes").innerHTML = 
+            document.getElementById("mensajes").innerHTML + data[x].mensaje + "<br>" +
+            data[x].fechahora + "<br>";
+        }
+    });
+} //leerM
+
+function tomarFoto() {
+    redactar.style.display = "none";
+    document.getElementById("mensajes").style.display = "none";
+    document.getElementById("camara").style.display = "block";
+    cerrarBarra();
+} //tomarFoto
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('../sw.js').then( () => {
+      console.log('Service Worker Registered')
+    })
+  })
+}
--- sw.js
+++ sw.js
@@ -1,28 +1,15 @@
 self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open('sw-cache').then(function(cache) {
-            return cache.add('index.html');
-        })
-    );
+  event.waitUntil(
+    caches.open('sw-cache').then(function(cache) {
+      return cache.add('index.html');
+    })
+  );
 });
  
 self.addEventListener('fetch', function(event) {
-event.respondWith(
+  event.respondWith(
     caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
+      return response || fetch(event.request);
     })
-);
-});
-
-self.addEventListener("message", async e=>{
-    if ("refresh" === e.data) {
-        await self.skipWaiting();
-        (await self.clients.matchAll({
-            type: "window",
-            includeUncontrolled: !0
-        })).forEach(e=>{
-            e.navigate(e.url)
-        }
-        )
-    }
+  );
 });
